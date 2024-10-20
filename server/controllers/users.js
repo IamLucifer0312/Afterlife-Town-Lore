7787be3a463a2678f8c0c52bdac2bbc20d41bda0
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-10-20 15:57:29 +0700

    update controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,13 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     password: { type: String, required: true },
-    stories: [String],
+    stories: [
+      {
+        title: String,
+        game: String,
+        content: String,
+      },
+    ],
   },
   {
     versionKey: false, // Prevents the `__v` field from being added
